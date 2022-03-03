cd642a21e37e32db2b60d6b6fa115e078e36a3db
DiegoBarrientos <dgbarrientos8ser@gmail.com>
2022-03-03 01:40:01 +0100

    implementacion test eliminarAlumno

---
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/CursoTest.java
@@ -0,0 +1,48 @@
+package com.iescomercio.ed.bloque2.repaso.modelo;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CursoTest {
+	private Curso c;
+	private Persona p;
+	@BeforeEach
+	void setUp() throws Exception {
+		c= new Curso();
+		p= new Persona("12345678D", "Diego", "Barrientos");
+		c.aniadirAlumno(p);
+	}
+
+	@Test
+	void testEliminarAlumno() throws Exception {
+		Exception e=assertThrows(Exception.class,
+				()-> c.eliminarAlumno("12414"));
+		assertEquals("El dni no tiene la longitud adecuada", e.getMessage());
+		c.eliminarAlumno(p.getDni());
+		assertFalse(c.estaRegistrado(p.getDni()));
+	}
+
+	@Test
+	void testAniadirAlumno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstaRegistrado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCurso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNumeroAlumnos() {
+		fail("Not yet implemented");
+	}
+
+}
